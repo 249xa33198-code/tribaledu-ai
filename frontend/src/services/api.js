54be5755a0463465fd73b64decc7ee5e970e00ac
249xa33198-code <249xa33198@gprec.ac.in>
2026-09-17 12:35:42 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: https://tribaledu-backend.onrender.com,
+  baseURL: 'https://tribaledu-backend.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
